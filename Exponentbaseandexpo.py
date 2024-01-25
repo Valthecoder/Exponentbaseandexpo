b391302db82ad7e36bc0b3ade2e36038d991fa4a
ValtheCoder <keshia.kdf@gmail.com>
2024-01-25 18:44:15 +0800

    Def function

--- Exponentbaseandexpo.py
+++ Exponentbaseandexpo.py
@@ -1,3 +1,9 @@
 # Case 1 and Case 2
 # Define Function
+def expo(base, exp):
+    num1 = exp
+    baseraise = 1
+    while num1 > 0:
+        baseraise = baseraise * base
+        num1 = num1 - 1
 # Print
