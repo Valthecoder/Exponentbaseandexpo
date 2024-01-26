24168dca5ac17607b867685e905c7b088e256627
ValtheCoder <keshia.kdf@gmail.com>
2024-01-26 10:18:21 +0800

    Resemblance of expected output

--- Exponentbaseandexpo.py
+++ Exponentbaseandexpo.py
@@ -3,18 +3,24 @@
 def expo(base, exp):
     num1 = exp
     baseraise = 1
+    base_exp = []
     while num1 > 0:
         baseraise = baseraise * base
+        base_exp.append(str(base))
         num1 = num1 - 1
+    base_timesexp = "({}, {})".format(' *'.join(base_exp), baseraise)
+
 # Print
     print("base =", base)
     print("exponent =", exp)
     print(base, "raise to the power of ", exp, end='')
     print(':', baseraise)
+    print("i.e.", base_timesexp)
+
 
 print("Case 1:")
 expo(2, 5)
 print("")
+print("")
 print("Case 2:")
 expo(5, 4)
-
